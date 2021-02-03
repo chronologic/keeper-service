5e11243d97d09645289c7c4975ed4be48a3a3344
Marcin <zolkiewski.marcin@gmail.com>
2021-02-04 00:46:00 +0100

    improved logging

--- src/services/userAccountingHelper.ts
+++ src/services/userAccountingHelper.ts
@@ -3,16 +3,21 @@ import { getConnection } from 'typeorm';
 
 import { Payment, User, UserDepositTxPayment } from '../entities';
 import { WARNING_USER_BALANCE_ETH } from '../env';
-import { numberToBnEth } from '../utils';
+import { createLogger } from '../logger';
+import { bnToNumberEth, numberToBnEth } from '../utils';
 import emailService from './emailService';
 import userHelper from './userHelper';
 
+const logger = createLogger('userAccountingHelper');
+
 async function updateAllUserBalances(): Promise<void> {
+  logger.info('Updating all user balances...');
   const users = await getConnection().createEntityManager().find(User);
 
   for (const user of users) {
     await updateUserBalance(user.id);
   }
+  logger.info('Updated all user balances.');
 }
 
 async function updateUserBalancesForDeposit(depositId: number): Promise<void> {
@@ -24,13 +29,23 @@ async function updateUserBalancesForDeposit(depositId: number): Promise<void> {
 }
 
 async function updateUserBalance(userId: number): Promise<BigNumber> {
-  const amountIn = await getUserPaymentsSum(userId);
-  const amountOut = await getUserDepositTxPaymentsSum(userId);
-  const balanceEth = amountIn.sub(amountOut);
+  try {
+    logger.info(`Updating user ${userId}'s balance...`);
 
-  await getConnection().createEntityManager().update(User, { id: userId }, { balanceEth });
+    const amountIn = await getUserPaymentsSum(userId);
+    const amountOut = await getUserDepositTxPaymentsSum(userId);
+    const balanceEth = amountIn.sub(amountOut);
 
-  return balanceEth;
+    await getConnection().createEntityManager().update(User, { id: userId }, { balanceEth });
+
+    logger.info(`Updated user ${userId}'s balance.`);
+
+    return balanceEth;
+  } catch (e) {
+    logger.error(e);
+  }
+
+  return BigNumber.from('0');
 }
 
 async function getUserDepositTxPaymentsSum(userId: number): Promise<BigNumber> {
@@ -41,7 +56,7 @@ async function getUserDepositTxPaymentsSum(userId: number): Promise<BigNumber> {
     .where('up."userId" = :userId', { userId })
     .execute();
 
-  return BigNumber.from(sum);
+  return BigNumber.from(sum || '0');
 }
 
 async function getUserPaymentsSum(userId: number): Promise<BigNumber> {
@@ -53,25 +68,49 @@ async function getUserPaymentsSum(userId: number): Promise<BigNumber> {
     .andWhere('p.status = :status', { status: Payment.Status.CONFIRMED })
     .execute();
 
-  return BigNumber.from(sum);
+  return BigNumber.from(sum || '0');
+}
+
+async function checkAllUserBalances(): Promise<void> {
+  logger.info('Checking all user balances...');
+  const users = await getConnection().createEntityManager().find(User);
+
+  for (const user of users) {
+    await checkUserBalance(user);
+  }
+  logger.info('Checked all user balances.');
 }
 
 async function checkUserBalancesForDeposit(depositId: number): Promise<void> {
+  logger.info(`Checking all user balances for deposit ${depositId}...`);
   const users = await userHelper.getUsersForDeposit(depositId);
 
   for (const user of users) {
     await checkUserBalance(user);
   }
+  logger.info(`Checked all user balances for deposit ${depositId}.`);
 }
 
 async function checkUserBalance(user: User): Promise<void> {
-  if (user.balanceEth.lte(numberToBnEth(WARNING_USER_BALANCE_ETH))) {
-    emailService.accountBalanceLow(user);
+  try {
+    logger.info(`Checking user ${user.id}'s balance...`);
+    if (user.balanceEth.lte(numberToBnEth(WARNING_USER_BALANCE_ETH))) {
+      logger.info(
+        `User ${user.id}'s balance is too low. Min: ${WARNING_USER_BALANCE_ETH}, current: ${bnToNumberEth(
+          user.balanceEth
+        )}`
+      );
+      emailService.accountBalanceLow(user);
+    }
+    logger.info(`Checked user ${user.id}'s balance.`);
+  } catch (e) {
+    logger.error(e);
   }
 }
 
 export default {
   updateAllUserBalances,
   updateUserBalancesForDeposit,
+  checkAllUserBalances,
   checkUserBalancesForDeposit,
 };
